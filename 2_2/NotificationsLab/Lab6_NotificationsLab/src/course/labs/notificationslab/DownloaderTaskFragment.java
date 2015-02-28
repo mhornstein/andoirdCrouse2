4881049ba88e8a8220126edaf062e1f862f290a2
mhornstein <mor.h100@gmail.com>
2015-02-28 20:47:25 +0200

    final version

--- 2_2/NotificationsLab/Lab6_NotificationsLab/src/course/labs/notificationslab/DownloaderTaskFragment.java
+++ 2_2/NotificationsLab/Lab6_NotificationsLab/src/course/labs/notificationslab/DownloaderTaskFragment.java
@@ -1,334 +1,258 @@
 package course.labs.notificationslab;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import android.app.Activity;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.app.Fragment;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.RemoteViews;
+import android.widget.RemoteViews.RemoteView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements SelectionListener,
-		DownloadFinishedListener {
+public class DownloaderTaskFragment extends Fragment {
 
-	private static final String TAG_NAME = "name";
-	private static final String TAG_USER = "user";
-	private static final String TAG_TEXT = "text";
-	private static final String TAG_FRIENDS_FRAGMENT = "friends_fragment";
-	private static final String TAG_FEED_FRAGMENT = "feed_fragment";
-	private static final String TAG_DOWNLOADER_FRAGMENT = "downloader_fragment";
-	private static final String TAG_IS_DATA_AVAILABLE = "is_data_available";
-	private static final String TAG_PROCESSED_FEEDS = "processed_feeds";
-	static final String TAG_TWEET_DATA = "data";
-	static final String TAG_FRIEND_RES_IDS = "friends";
+	private DownloadFinishedListener mCallback;
+	private Context mContext;
+	private final int MY_NOTIFICATION_ID = 11151990;
 
-	public static final String TWEET_FILENAME = "tweets.txt";
-	public final static String[] FRIENDS_NAMES = { "taylorswift13",
-			"msrebeccablack", "ladygaga" };
-	public static final int IS_ALIVE = Activity.RESULT_FIRST_USER;
-	public static final String DATA_REFRESHED_ACTION = "course.labs.notificationslabnew.DATA_REFRESHED";
+	@SuppressWarnings("unused")
 	private static final String TAG = "Lab-Notifications";
 
-	// Raw feed file IDs used to reference stored tweet data
-	public static final ArrayList<Integer> sRawTextFeedIds = new ArrayList<Integer>(
-			Arrays.asList(R.raw.tswift, R.raw.rblack, R.raw.lgaga));
-
-	private FragmentManager mFragmentManager;
-	private FriendsFragment mFriendsFragment;
-	private FeedFragment mFeedFragment;
-	private DownloaderTaskFragment mDownloaderFragment;
-	private boolean mIsInteractionEnabled;
-	private String[] mFormattedFeeds = new String[sRawTextFeedIds.size()];;
-	private boolean mIsFresh;
-	private BroadcastReceiver mRefreshReceiver;
-	private static final long TWO_MIN = 2 * 60 * 1000;
-
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
 
-		mFragmentManager = getFragmentManager();
+		// Preserve across reconfigurations
+		setRetainInstance(true);
 
-		// Reset instance state on reconfiguration
-		if (null != savedInstanceState) {
-			restoreState(savedInstanceState);
-		} else {
-			setupFragments();
+		// TODO: Create new DownloaderTask that "downloads" data
+		AsyncTask<Integer[], Integer, String[]> downloaderTask = new DownloaderTask();
+
+		// TODO: Retrieve arguments from DownloaderTaskFragment
+		// Prepare them for use with DownloaderTask. 
+		Integer[] feedIds = MainActivity.sRawTextFeedIds.toArray(new Integer[MainActivity.sRawTextFeedIds.size()]);
+
+		// TODO: Start the DownloaderTask 
+		downloaderTask.execute(feedIds);
+	}
+
+	// Assign current hosting Activity to mCallback
+	// Store application context for use by downloadTweets()
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+
+		mContext = activity.getApplicationContext();
+
+		// Make sure that the hosting activity has implemented
+		// the correct callback interface.
+		try {
+			mCallback = (DownloadFinishedListener) activity;
+		} catch (ClassCastException e) {
+			throw new ClassCastException(activity.toString()
+					+ " must implement DownloadFinishedListener");
 		}
 	}
 
-	// One time setup of UI and retained (headless) Fragment
-	private void setupFragments() {
-		installFriendsFragment();
+	// Null out mCallback
+	@Override
+	public void onDetach() {
+		super.onDetach();
+		mCallback = null;
+	}
 
-		// The feed is fresh if it was downloaded less than 2 minutes ago
-		mIsFresh = (System.currentTimeMillis() - getFileStreamPath(
-				TWEET_FILENAME).lastModified()) < TWO_MIN;
-		if (!mIsFresh) {
-			installDownloaderTaskFragment();
+	// TODO: Implement an AsyncTask subclass called DownLoaderTask.
+	// This class must use the downloadTweets method (currently commented
+	// out). Ultimately, it must also pass newly available data back to
+	// the hosting Activity using the DownloadFinishedListener interface.
 
-			// TODO: Show a Toast message displaying
-			// R.string.download_in_progress_string
-			String msg = getString(R.string.download_in_progress_string);
-			Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
-			
-			// Set up a BroadcastReceiver to receive an Intent when download
-			// finishes.
-			mRefreshReceiver = new BroadcastReceiver() {
-				@Override
-				public void onReceive(Context context, Intent intent) {
+	public class DownloaderTask extends AsyncTask<Integer[], Integer, String[]> {
 
-					// TODO:
-					// Check to make sure this is an ordered broadcast
-					// Let sender know that the Intent was received
-					// by setting result code to MainActivity.IS_ALIVE
-					if (isOrderedBroadcast()) {
-						setResultCode(MainActivity.IS_ALIVE);
+		@Override
+		protected String[] doInBackground(Integer[]... params) {
+			String[] tweets = downloadTweets(params[0]);
+			return tweets;
+		}
+
+		@Override
+		protected void onPostExecute(String[] result) {
+			if(isAdded()) {
+				mCallback.notifyDataRefreshed(result);
+			}
+		}
+
+
+		private String[] downloadTweets(Integer resourceIDS[]) {
+
+			final int simulatedDelay = 2000;
+			String[] feeds = new String[resourceIDS.length];
+			boolean downLoadCompleted = false;
+
+			try {
+				for (int idx = 0; idx < resourceIDS.length; idx++) {
+					InputStream inputStream;
+					BufferedReader in;
+					try {
+						// Pretend downloading takes a long time
+						Thread.sleep(simulatedDelay);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+
+					inputStream = mContext.getResources().openRawResource(
+							resourceIDS[idx]);
+					in = new BufferedReader(new InputStreamReader(inputStream));
+
+					String readLine;
+					StringBuffer buf = new StringBuffer();
+
+					while ((readLine = in.readLine()) != null) {
+						buf.append(readLine);
+					}
+
+					feeds[idx] = buf.toString();
+
+					if (null != in) {
+						in.close();
 					}
 				}
-			};
 
-		} else {
-			// Process Twitter data taken from stored file
-			parseJSON(loadTweetsFromFile());
+				downLoadCompleted = true;
+				saveTweetsToFile(feeds);
 
-			// Enable user interaction
-			mIsInteractionEnabled = true;
-		}
-	}
-
-	// Add Friends Fragment to Activity
-	private void installFriendsFragment() {
-
-		// Make new Fragment
-		mFriendsFragment = new FriendsFragment();
-
-		// Give Fragment to the FragmentManager
-		FragmentTransaction transaction = mFragmentManager.beginTransaction();
-		transaction.replace(R.id.fragment_container, mFriendsFragment,
-				TAG_FRIENDS_FRAGMENT);
-		transaction.commit();
-	}
-
-	// Add DownloaderTaskFragment to Activity
-	private void installDownloaderTaskFragment() {
-
-		// Make new Fragment
-		mDownloaderFragment = new DownloaderTaskFragment();
-
-		// Set DownloaderTaskFragment arguments
-		Bundle args = new Bundle();
-		args.putIntegerArrayList(TAG_FRIEND_RES_IDS, sRawTextFeedIds);
-		mDownloaderFragment.setArguments(args);
-
-		// Give Fragment to the FragmentManager
-		mFragmentManager.beginTransaction()
-				.add(mDownloaderFragment, TAG_DOWNLOADER_FRAGMENT).commit();
-	}
-
-	// Register the BroadcastReceiver
-	@Override
-	protected void onResume() {
-		super.onResume();
-
-		// TODO:
-		// Register the BroadcastReceiver to receive a
-		// DATA_REFRESHED_ACTION broadcast
-		IntentFilter filter = new IntentFilter(DATA_REFRESHED_ACTION);
-		registerReceiver(mRefreshReceiver, filter);
-	}
-
-	@Override
-	protected void onPause() {
-
-		// TODO:
-		// Unregister the BroadcastReceiver if it has been registered
-		// Note: check that mRefreshReceiver is not null before attempting to
-		// unregister in order to work around an Instrumentation issue
-		if(mRefreshReceiver != null) {
-			unregisterReceiver(mRefreshReceiver);
-		}
-		
-		super.onPause();
-
-	}
-
-	/*
-	 * DownloadFinishedListener method
-	 */
-
-	// Called back by DownloaderTask after data has been loaded
-	public void notifyDataRefreshed(String[] feeds) {
-
-		// Process downloaded data
-		parseJSON(feeds);
-
-		// Enable user interaction
-		mIsInteractionEnabled = true;
-		allowUserClicks();
-
-	};
-
-	// Enable user interaction with FriendFragment
-	private void allowUserClicks() {
-		mFriendsFragment.setAllowUserClicks(true);
-	}
-
-	/*
-	 * SelectionListener methods
-	 */
-
-	// Report whether users interaction is enabled
-	public boolean canAllowUserClicks() {
-		return mIsInteractionEnabled;
-	}
-
-	// Installs the FeedFragment when a Friend name is
-	// selected in the FriendsFragment
-	@Override
-	public void onItemSelected(int position) {
-		installFeedFragment(mFormattedFeeds[position]);
-	}
-
-	// Add FeedFragment to Activity
-	private void installFeedFragment(String tweetData) {
-		// Make new Fragment
-		mFeedFragment = new FeedFragment();
-
-		// Set Fragment arguments
-		Bundle args = new Bundle();
-		args.putString(TAG_TWEET_DATA, tweetData);
-		mFeedFragment.setArguments(args);
-
-		// Give Fragment to the FragmentManager
-		FragmentTransaction transaction = mFragmentManager.beginTransaction();
-		transaction.replace(R.id.fragment_container, mFeedFragment,
-				TAG_FEED_FRAGMENT);
-		transaction.addToBackStack(null);
-		transaction.commit();
-	}
-
-	@Override
-	protected void onSaveInstanceState(Bundle savedInstanceState) {
-		if (null != mFriendsFragment) {
-			savedInstanceState.putString(TAG_FRIENDS_FRAGMENT,
-					mFriendsFragment.getTag());
-		}
-		if (null != mFeedFragment) {
-			savedInstanceState.putString(TAG_FEED_FRAGMENT,
-					mFeedFragment.getTag());
-		}
-		if (null != mDownloaderFragment) {
-			savedInstanceState.putString(TAG_DOWNLOADER_FRAGMENT,
-					mDownloaderFragment.getTag());
-		}
-		savedInstanceState.putBoolean(TAG_IS_DATA_AVAILABLE,
-				mIsInteractionEnabled);
-		savedInstanceState.putStringArray(TAG_PROCESSED_FEEDS, mFormattedFeeds);
-
-		super.onSaveInstanceState(savedInstanceState);
-
-	}
-
-	// Restore saved instance state
-	private void restoreState(Bundle savedInstanceState) {
-
-		// Fragments tags were saved in onSavedInstanceState
-		mFriendsFragment = (FriendsFragment) mFragmentManager
-				.findFragmentByTag(savedInstanceState
-						.getString(TAG_FRIENDS_FRAGMENT));
-
-		mFeedFragment = (FeedFragment) mFragmentManager
-				.findFragmentByTag(savedInstanceState
-						.getString(TAG_FEED_FRAGMENT));
-
-		mDownloaderFragment = (DownloaderTaskFragment) mFragmentManager
-				.findFragmentByTag(savedInstanceState
-						.getString(TAG_DOWNLOADER_FRAGMENT));
-
-		mIsInteractionEnabled = savedInstanceState
-				.getBoolean(TAG_IS_DATA_AVAILABLE);
-		if (mIsInteractionEnabled) {
-			mFormattedFeeds = savedInstanceState
-					.getStringArray(TAG_PROCESSED_FEEDS);
-		}
-	}
-
-	// Convert raw data (in JSON format) into text for display
-	private void parseJSON(String[] feeds) {
-		JSONArray[] JSONFeeds = new JSONArray[feeds.length];
-		for (int i = 0; i < JSONFeeds.length; i++) {
-			try {
-				JSONFeeds[i] = new JSONArray(feeds[i]);
-			} catch (JSONException e) {
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 
-			String name = "";
-			String tweet = "";
-			JSONArray tmp = JSONFeeds[i];
+			// Notify user that downloading has finished
+			notify(downLoadCompleted);
 
-			// string buffer for feeds
-			StringBuffer tweetRec = new StringBuffer("");
-			for (int j = 0; j < tmp.length(); j++) {
-				try {
-					tweet = tmp.getJSONObject(j).getString(TAG_TEXT);
-					JSONObject user = (JSONObject) tmp.getJSONObject(j).get(
-							TAG_USER);
-					name = user.getString(TAG_NAME);
-				} catch (JSONException e) {
-					e.printStackTrace();
-				}
-				tweetRec.append(name + " - " + tweet + "\n\n");
-			}
-			mFormattedFeeds[i] = tweetRec.toString();
+			return feeds;
+
 		}
-	}
+		private void notify(final boolean success) {
 
-	// Retrieve feeds text from a file
-	// Store them in mRawTextFeed[]
+			final Intent restartMainActivityIntent = new Intent(mContext,
+					MainActivity.class);
+			restartMainActivityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-	private String[] loadTweetsFromFile() {
-		BufferedReader reader = null;
-		ArrayList<String> rawFeeds = new ArrayList<String>();
-		try {
-			FileInputStream fis = openFileInput(TWEET_FILENAME);
-			reader = new BufferedReader(new InputStreamReader(fis));
-			String s = null;
-			int i = 0;
-			while (null != (s = reader.readLine())) {
-				rawFeeds.add(i, s);
-				i++;
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (null != reader) {
-				try {
-					reader.close();
-				} catch (IOException e) {
-					e.printStackTrace();
+			// Sends an ordered broadcast to determine whether MainActivity is
+			// active and in the foreground. Creates a new BroadcastReceiver
+			// to receive a result indicating the state of MainActivity
+
+			// The Action for this broadcast Intent is
+			// MainActivity.DATA_REFRESHED_ACTION
+			// The result, MainActivity.IS_ALIVE, indicates that MainActivity is
+			// active and in the foreground.
+
+			mContext.sendOrderedBroadcast(new Intent(
+					MainActivity.DATA_REFRESHED_ACTION), null,
+					new BroadcastReceiver() {
+
+				final String failMsg = mContext
+						.getString(R.string.download_failed_string);
+				final String successMsg = mContext
+						.getString(R.string.download_succes_string);
+				final String notificationSentMsg = mContext
+						.getString(R.string.notification_sent_string);
+
+				@Override
+				public void onReceive(Context context, Intent intent) {
+
+					int resultCode = getResultCode();
+
+					if (resultCode != MainActivity.IS_ALIVE) {
+
+						// TODO: If not, create a PendingIntent using
+						// the
+						// restartMainActivityIntent and set its flags
+						// to FLAG_UPDATE_CURRENT
+						PendingIntent pendingIntent = PendingIntent.getActivity(mContext, 0,
+								restartMainActivityIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+						// Uses R.layout.custom_notification for the
+						// layout of the notification View. The xml
+						// file is in res/layout/custom_notification.xml
+
+						RemoteViews mContentView = new RemoteViews(
+								mContext.getPackageName(),
+								R.layout.custom_notification);
+
+						// TODO: Set the notification View's text to
+						// reflect whether the download completed
+						// successfully
+						
+						String msg = success ? successMsg : failMsg;
+						mContentView.setTextViewText(R.id.text, msg);
+
+						// TODO: Use the Notification.Builder class to
+						// create the Notification. You will have to set
+						// several pieces of information. You can use
+						// android.R.drawable.stat_sys_warning
+						// for the small icon. You should also
+						// setAutoCancel(true).
+
+						Notification.Builder notificationBuilder = new Notification.Builder(
+								mContext)
+							.setSmallIcon(android.R.drawable.stat_sys_warning)
+							.setAutoCancel(true)
+							.setContentIntent(pendingIntent)
+							.setContent(mContentView);
+						
+						// TODO: Send the notification
+						
+						Notification notification = notificationBuilder.build();
+						
+						NotificationManager mNotificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+						mNotificationManager.notify(MY_NOTIFICATION_ID,
+								notificationBuilder.build());
+
+						Toast.makeText(mContext, notificationSentMsg,
+								Toast.LENGTH_LONG).show();
+
+					} else {
+						Toast.makeText(mContext,
+								success ? successMsg : failMsg,
+										Toast.LENGTH_LONG).show();
+					}
+				}
+			}, null, 0, null, null);
+		}
+
+		private void saveTweetsToFile(String[] result) {
+			PrintWriter writer = null;
+			try {
+				FileOutputStream fos = mContext.openFileOutput(
+						MainActivity.TWEET_FILENAME, Context.MODE_PRIVATE);
+				writer = new PrintWriter(new BufferedWriter(
+						new OutputStreamWriter(fos)));
+
+				for (String s : result) {
+					writer.println(s);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				if (null != writer) {
+					writer.close();
 				}
 			}
 		}
-		return rawFeeds.toArray(new String[rawFeeds.size()]);
 	}
 }
